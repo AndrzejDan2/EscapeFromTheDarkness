dfe4734b2c206c3bf00492fd53fd1d0e6010d1bb
Andrzej <s188914@student.pg.edu.pl>
2023-11-11 21:47:29 +0100

    Add Game class

---
+++ src/Game.java
@@ -0,0 +1,6 @@
+public class Game {
+    public Game() {
+        GameArea ga = new GameArea();
+        GameWindow gw = new GameWindow(ga);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        GameArea ga = new GameArea();
-        GameWindow gw = new GameWindow(ga);
+        Game game = new Game();
 
     }
 }
