1eeb0520574baac1a222a422b16800cef444ba3d
Andrzej <s188914@student.pg.edu.pl>
2023-11-12 00:06:27 +0100

    Add Button class

---
+++ src/Button.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Button extends JButton {
+    private final String text;
+    private final int x,y;
+    private final int width, height;
+
+    public Button(String text, int x, int y, int width, int height) {
+        super(text);
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        setBounds(this.x, this.y, this.width, this.height);
+    }
+
+
+}
