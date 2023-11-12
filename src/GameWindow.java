d427177e9569d12c9bc734140984b91afdd75798
Andrzej <s188914@student.pg.edu.pl>
2023-11-12 22:29:08 +0100

    [GameWindow] Change JWindow into JFrame

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GameWindow extends JWindow{
+public class GameWindow extends JFrame{
     final int windowWidth = 1024;
     final int windowHeight = 768;
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -9,6 +9,7 @@ public class GameWindow extends JWindow{
         super();
         setSize(windowWidth, windowHeight);
         setLocation((screenSize.width- windowWidth)/2, (screenSize.height- windowHeight)/2);
+        setUndecorated(true);
         setLayout(null); // bez tego setBounds dla GameArea sie nie ustawi
         Menu menu = new Menu();
         add(menu);
