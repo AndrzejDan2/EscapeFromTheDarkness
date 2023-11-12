23bf588988cf42265f197348e6a45fc8e734b22d
Andrzej <s188914@student.pg.edu.pl>
2023-11-12 17:07:37 +0100

    [Button] Add custom icons v1 for buttons

--- src/Menu.java
+++ src/Menu.java
@@ -4,16 +4,16 @@ import java.awt.*;
 public class Menu extends JPanel {
     public Menu() {
         super();
-        setBackground(Color.lightGray);
+        setBackground(Color.WHITE);
         setBounds(0,0, 1024, 68);
         setLayout(null);
         initButtons();
     }
     public void initButtons(){
-        Button bPlay = new Button("Play", 40, 10, 50, 50);
-        Button bPause = new Button("Pause", 110, 10, 50, 50);
-        Button bRestart = new Button("Restart", 180, 10, 50, 50);
-        Button bExit = new Button("Exit", 250, 10, 50, 50);
+        Button bPlay = new Button("Play", 40, 10, 50, 50,  new ImageIcon("res/Play.png"));
+        Button bPause = new Button("Pause", 110, 10, 50, 50, new ImageIcon("res/Pause.png"));
+        Button bRestart = new Button("Restart", 180, 10, 50, 50, new ImageIcon("res/Restart.png"));
+        Button bExit = new Button("Exit", 250, 10, 50, 50, new ImageIcon("res/Exit.png"));
         this.add(bPlay);
         this.add(bPause);
         this.add(bRestart);
